1548cf8b2965658e8e7c96f660f21a83edf5dd2d
jackAvilaP <arenca44@gmail.com>
2022-06-10 12:37:04 -0500

    creacion User

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
 import { Login, CheckIn, User } from './components';
-=======
-import { Login, CheckIn } from './components';
->>>>>>> 48f3621fac2acfa2a48fcd1412bde5b3f7219089
-
 import './App.css';
 
 function App() {
 
   return (
     <div className="App">
-<<<<<<< HEAD
       <User/>
-=======
-      <CheckIn/>
->>>>>>> 48f3621fac2acfa2a48fcd1412bde5b3f7219089
       <footer/>
     </div>
   );
